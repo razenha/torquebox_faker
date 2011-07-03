e933213d1b57bca6c9408668e76007f364ee5d2f
razenha <rubem.azenha@gmail.com>
2011-07-03 01:53:38 -0300

    Faking Tasks

--- lib/task.rb
+++ lib/task.rb
@@ -2,10 +2,7 @@ module TorqueBox
   module Messaging
     class Task
       def self.async(method_name, params = Hash.new)
-        puts "Entrou aqui: " + self.class.to_s + "." + method_name.to_s
         Thread.new {
-          puts "Criou thread: " + self.class.to_s + "." + method_name.to_s
-          
           self.new.send(method_name, params)
         }
       end
